eee06b857df5162bf8260828f76e0f95da01bb81
ADO <ado@oxalide.com>
2018-01-30 13:38:34 +0100

    Add terraform configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fargate with Terraform
+
+Example repository to deploy an ECS cluster hosting a web application.
---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "aws_account_id" {
+  description = "AWS account ID"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "adongy/hostname-docker:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 3000
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 2
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "512"
+}
